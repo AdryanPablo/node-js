8e0abbb1ea762506a566769ec31b16290292a8d7
Ádryan Pablo <pabloadryan5@gmail.com>
2022-01-20 12:44:31 -0300

    Objetos em EJS

--- EJS/server.js
+++ EJS/server.js
@@ -4,12 +4,42 @@ const app = express()
 app.set("view engine", "ejs")
 
 app.get("/", function (resquest, response) {
-    response.render("index")
+    const items = [
+        {
+            title: 'D',
+            message: 'esenvolver soluções'
+        },
+        {
+            title: 'E',
+            message: 'voluir'
+        },
+        {
+            title: 'M',
+            message: 'arcar as pessoas'
+        },
+        {
+            title: 'A',
+            message: 'prender sempre'
+        },
+        {
+            title: 'I',
+            message: 'r mais longe'
+        },
+        {
+            title: 'S',
+            message: 'aber o seu lugar'
+        }
+    ]
+    const subtitle = 'O EJS é uma linguagem de modelagem para criação de página HTML'
+    response.render("pages/index", {
+        acronym: items,
+        subtitle: subtitle
+    })
 })
 
 app.get("/about", function (resquest, response) {
-    response.render("about") 
+    response.render("pages/about") 
 })
 
 app.listen(8080)
-console.log('Rodando...');
+console.log('Servidor funcionando!')
